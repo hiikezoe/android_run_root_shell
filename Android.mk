90cc8d2b9f2c9c26e3ae2a539b9598d57a6847b0
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-05-21 22:46:19 +0900

    Prepare for use of libdiagexploit

--- Android.mk
+++ Android.mk
@@ -9,6 +9,9 @@ LOCAL_SRC_FILES := \
 LOCAL_MODULE := run_root_shell
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libdiagexploit
 LOCAL_STATIC_LIBRARIES += libcutils libc
 
 include $(BUILD_EXECUTABLE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
