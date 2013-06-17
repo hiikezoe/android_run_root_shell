98b658ca9c037a8ae4dbf984b75957a791489a9d
DooMLoRD <metoo.mememe@gmail.com>
2013-06-18 03:49:14 +0530

    added C5302 1272-1092 12.0.A.1.284

--- device_database.c
+++ device_database.c
@@ -27,6 +27,7 @@ static supported_device supported_devices[] = {
   { DEVICE_LT26W_1265_3909_6_2_B_0_200, "LT26w",        "6.2.B.0.200",       "ro.semc.version.sw",          "1265-3909" },
   { DEVICE_LT26I_1257_8080_6_2_B_0_211, "LT26i",	"6.2.B.0.211",       "ro.semc.version.cust",          "1257-8080" },
   { DEVICE_C6603_1269_5309_10_1_1_A_1_307, "C6603",	"10.1.1.A.1.307",    "ro.semc.version.cust",          "1270-6704" },
+  { DEVICE_C5302_1272_1092_12_0_A_1_284, "C5302",	"12.0.A.1.284",      "ro.semc.version.cust",          "1272-1092" },
   { DEVICE_N05E_A1000311,           "N05E",             "A1000311"           },
   { DEVICE_SC04E_OMUAMDI,           "SC-04E"            "JDQ39.SC04EOMUAMDI" },
   { DEVICE_SCL21_KDALJD,            "SCL21",            "IMM76D.SCL21KDALJD" },
