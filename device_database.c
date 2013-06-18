1b2c30320832cbe36805b3b15a0f7a15a285b499
fi01 <furu.is01@gmail.com>
2013-06-18 09:57:47 +0900

    Use prop ro.semc.version.sw to detect Xperia LT26w

--- device_database.c
+++ device_database.c
@@ -24,7 +24,7 @@ static supported_device supported_devices[] = {
   { DEVICE_L02E_V10c,               "L-02E",            "IMM76L",            "ro.build.version.incremental",  "L02E10c.1354024955" },
   { DEVICE_L02E_V10e,               "L-02E",            "IMM76L",            "ro.build.version.incremental",  "L02E10e.1366099439" },
   { DEVICE_L06D_V10k,               "L-06D",            "IMM76D",            "ro.build.version.incremental",  "L06DV10k.4821c158" },
-  { DEVICE_LT26W_1266_3278_6_2_B_0_200, "LT26w",        "6.2.B.0.200",       "ro.semc.version.cust",          "1266-3278" },
+  { DEVICE_LT26W_1265_3909_6_2_B_0_200, "LT26w",        "6.2.B.0.200",       "ro.semc.version.sw",          "1265-3909" },
   { DEVICE_LT26I_1257_8080_6_2_B_0_211, "LT26i",	"6.2.B.0.211",       "ro.semc.version.cust",          "1257-8080" },
   { DEVICE_C6603_1269_5309_10_1_1_A_1_307, "C6603",	"10.1.1.A.1.307",    "ro.semc.version.cust",          "1270-6704" },
   { DEVICE_N05E_A1000311,           "N05E",             "A1000311"           },
